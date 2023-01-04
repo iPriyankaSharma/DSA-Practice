708d04b92cf516d669b41c7b7b5a2667fe928ce5
Priyanka Sharma <priyanka.sharma.19903@gmail.com>
2023-01-04 23:53:11 +0530

    Add files via upload

---
+++ String/LongestCommonPrefix.js
@@ -0,0 +1,28 @@
+// Problem statement - in given series of words, which words is longest common eg.
+// hard, hard, had - ha is longest common word. Or one, out, only - o is longest common prefix.
+
+// Method 1 - Double loop - match each str char
+
+let longestCommonPrefix = function (str) {
+  if (!str.length) return "";
+
+  for (let i = 0; i <= str[0].length; i++) {
+    for (let j = 1; j < str.length; j++) {
+      if (str[0][i] !== str[j][i]) {
+        return str[0].slice(0, i);
+      }
+    }
+  }
+};
+
+// Method 2 - Array.every() - same as method 1 but second array is omitted.
+
+let longestCommon = function (str) {
+  if (!str.length) return "";
+
+  for (let i = 0; i <= str[0].length; i++) {
+    if (!str.every((string) => str[i] == str[o][i])) {
+      return str[0].slice(0, i);
+    }
+  }
+};
